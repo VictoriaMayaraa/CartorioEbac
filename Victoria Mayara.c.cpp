294603cbf1151324f0a8e383045b20cca9921af3
VictoriaMayaraa <victoriamaylima@gmail.com>
2024-07-25 15:48:31 -0300

    Opção de saída
    Incluso no sistema a opção de saida para o usuário

--- Victoria Mayara.c.cpp
+++ Victoria Mayara.c.cpp
@@ -118,6 +118,7 @@ int main ()
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar os nomes\n");
         printf("\t3 - Deletar os nomes\n");
+        printf("\t4 - Sair do sistema\n\n ");
 
         scanf("%d", &opcao);
         system("cls");
@@ -134,6 +135,11 @@ int main ()
 
             case 3:
                deletar();
+                break;
+                
+                case 4:
+                printf(" Obrigado por Utilizar o sistema\n");
+                return 0;
                 break;
 
             default:
